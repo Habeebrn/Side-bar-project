2e3f05eeb7c86e3f1aa5ea88bcb7b910d8880da2
habeebrn <habeebrn.id@gmail.com>
2023-11-07 13:26:46 +0530

    Side bar project

---
+++ SideBar.js
@@ -0,0 +1,19 @@
+
+const tuggleBtn = document.querySelector(".sidebar-toggle")
+const sidebar = document.querySelector(".sidebar")
+const closeBtn = document.querySelector(".close-btn") 
+
+tuggleBtn.addEventListener("click", function (){
+    // if (sidebar.classList.contains("show-sidebar")) {
+    //     sidebar.classList.remove("show-sidebar")
+    // } else {
+    //     sidebar.classList.add("show-sidebar")
+    // }
+
+    sidebar.classList.toggle("show-sidebar")
+})
+
+closeBtn.addEventListener("click", function (){
+    sidebar.classList.remove("show-sidebar")
+})
+
